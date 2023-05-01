cd56b12d0c94072e90bd5bb9096e0717c302b3c5
Gyumin Choi <choegyumin@gmail.com>
2023-05-01 15:48:49 +0900

    chore: support type checking on dev

--- next.config.js
+++ next.config.js
@@ -1,7 +1,24 @@
+const ForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin');
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   pageExtensions: ['page.tsx', 'page.ts', 'page.jsx', 'page.js'],
+  webpack(config, options) {
+    const { dev, isServer } = options;
+    // Do not run type checking twice:
+    if (dev && !isServer) {
+      config.plugins.push(
+        new ForkTsCheckerWebpackPlugin({
+          devServer: dev,
+          issue: {
+            exclude: [{ file: '**/__mocks__/**/*' }, { file: '**/*.test.{js,jsx,ts,tsx}' }],
+          },
+        }),
+      );
+    }
+    return config;
+  },
   eslint: {
     ignoreDuringBuilds: true,
   },
